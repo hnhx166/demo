c7df3fccc4ba5326cb1687a4d4a46cf0fcebe352
hnhx166 <986895336@qq.com>
2018-03-28 16:31:23 +0800

    代码更新

--- src/main/java/com/chnghx/web/common/properties/MemberProperties.java
+++
@@ -1,7 +0,0 @@
-package com.chnghx.web.common.properties;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MemberProperties {}
